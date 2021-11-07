f694ffc47b6fe460b0bf4ec264f96e995f5bbd5b
Saira <vijay.rathore@ekaplus.com>
2021-11-07 15:42:55 +0530

    new code

--- src/main/java/com/college/controller/CollegeUIController.java
+++ src/main/java/com/college/controller/CollegeUIController.java
@@ -2,19 +2,29 @@ package com.college.controller;
 
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.college.entity.User;
 import com.college.repo.UserRepo;
+import com.college.service.UserService;
 
 @Controller
 public class CollegeUIController {
 
 	@Autowired
 	private UserRepo userRepo;
+
+	@Autowired
+	private UserService userService;
 
 	@GetMapping({ "/", "/login" })
 	public String getLogin() {
@@ -31,17 +41,27 @@ public class CollegeUIController {
 		return "signup";
 	}
 
+	@GetMapping("/success")
+	public String getSuccess() {
+		return "signupCompletion";
+	}
+
 	@GetMapping("/{token}/activate/account")
 	public String getSuccessPage(@PathVariable("token") String token) {
 		Optional<User> user = userRepo.findByToken(token);
 		if (user.isPresent()) {
-			User user1=user.get();
+			User user1 = user.get();
 			user1.setIsActive(true);
 			user1.setToken(null);
 			userRepo.save(user1);
 			return "success";
 		}
 		return "errorPage";
+	}
+
+	@GetMapping("/signupCompletion")
+	public String saveUserInfo() {
+		return "signupCompletion";
 	}
 
 }
